d7e6b76a1e2dfcb9d6dd2bc17ed69121a60f881e
AMIOUR Billel <b.amiour@gpcdz.com>
2021-03-29 10:27:04 +0100

    add some func

--- src/main/java/dz/m2gl/al/ProxyServiceApplication.java
+++ src/main/java/dz/m2gl/al/ProxyServiceApplication.java
@@ -2,7 +2,10 @@ package dz.m2gl.al;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -11,6 +14,16 @@ public class ProxyServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProxyServiceApplication.class, args);
+	}
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+			.route("course-service", r -> r.path("/course/**")
+				.uri("http://localhost:8001"))
+			.route("teacher-service", r -> r.path("/teacher/**")
+					.uri("http://localhost:8002/"))
+			
+			.build();
 	}
 
 }
